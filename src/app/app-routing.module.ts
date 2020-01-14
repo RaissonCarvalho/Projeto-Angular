c2f4de677e56bfe4727329e1c6a54bd68c8214c2
raissoncarvalho <raissoncarvalho94@gmail.com>
2020-01-14 20:38:22 -0300

    removendo components e services não necessários

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ProfileComponent } from "./profile/profile.component";
-import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 import { AdComponent } from './ad/ad.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
@@ -12,13 +10,11 @@ import { CreateAdComponent } from './create-ad/create-ad.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'ads', pathMatch: 'full' },
-  { path: 'profile-detail/:id', component: ProfileDetailComponent },
-  { path: 'profiles', component: ProfileComponent },
   { path: 'ads' , component: AdComponent, canActivate: [AuthGuard]},
   {path: 'ad-detail/:id', component: AdDetailComponent, canActivate: [AuthGuard]},
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'create-ad', component: CreateAdComponent },
+  { path: 'create-ad', component: CreateAdComponent, canActivate:[AuthGuard] },
 ];
 
 @NgModule({
